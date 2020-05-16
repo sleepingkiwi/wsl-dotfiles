4e324ddc31279676d7e8f511f08a19a92c15a357
sleepingkiwi <joe@sleeping.kiwi>
2020-05-16 15:27:32 +0100

    Make colours less gross, Add ghost

--- _wsl-dev-backup.sh
+++ _wsl-dev-backup.sh
@@ -8,7 +8,7 @@
 
 TODAY=`date "+%Y-%m-%d--%H-%M-%S"`
 
-echo -e "\n\033[0;90m--------------------\033[0m\n\033[0;32m    starting to archive ~/dev\n\033[0m\033[0;90m${TODAY}\n--------------------\033[0m\n"
+echo -e "\n\033[0;90m--------------------\033[0m\n\033[0;32m    👻 starting to archive ~/dev\n\033[0m\033[0;90m${TODAY}\n--------------------\033[0m\n"
 
 # remove previous backup if it exists
 \rm /home/sleepingkiwi/dev.tar.gz
@@ -17,7 +17,7 @@ echo -e "\n\033[0;90m--------------------\033[0m\n\033[0;32m    starting to arch
 tar -zcvf /home/sleepingkiwi/dev.tar.gz /home/sleepingkiwi/dev
 
 # archive complete - notify and move
-echo -e "\n\033[0;90m--------------------\033[0m\n\033[0;32m     ~/dev was archived and compressed. Moving to /s/Dropbox/_tedworth/_dev-backups/ubuntu-dev/dev--${TODAY}.tar.gz\n\033[0m\033[0;90m--------------------\033[0m\n"
+echo -e "\n\033[0;90m--------------------\033[0m\n\033[0;32m     👻 ~/dev was archived and compressed. Moving to /s/Dropbox/_tedworth/_dev-backups/ubuntu-dev/dev--${TODAY}.tar.gz\n\033[0m\033[0;90m--------------------\033[0m\n"
 
 # move archive into Dropbox
 
@@ -26,8 +26,8 @@ mv /home/sleepingkiwi/dev.tar.gz /s/Dropbox/_tedworth/_dev-backups/ubuntu-dev/de
 # cleanly exit
 FILE=/s/Dropbox/_tedworth/_dev-backups/ubuntu-dev/dev--${TODAY}.tar.gz
 if [ ! -e "$FILE" ]; then
-    echo -e "\n\033[0;90m--------------------\033[0m\n\033[0;31m     cannot find the file /home/sleepingkiwi/dev.tar.gz /s/Dropbox/_tedworth/_dev-backups/ubuntu-dev/dev--${TODAY}.tar.gz - something's gone wrong...\n\033[0m\033[0;90m${TODAY}\n--------------------\033[0m\n"
+    echo -e "\n\033[0;90m--------------------\033[0m\n\033[0;31m     👻 cannot find the file /home/sleepingkiwi/dev.tar.gz /s/Dropbox/_tedworth/_dev-backups/ubuntu-dev/dev--${TODAY}.tar.gz - something's gone wrong...\n\033[0m\033[0;90m${TODAY}\n--------------------\033[0m\n"
 else
-    echo -e "\n\033[0;90m--------------------\033[0m\n\033[0;32m     it has been done.\n\033[0m\033[0;90m${TODAY}\n--------------------\033[0m\n"
+    echo -e "\n\033[0;90m--------------------\033[0m\n\033[0;32m     👻 it has been done.\n\033[0m\033[0;90m${TODAY}\n--------------------\033[0m\n"
 fi
 exit 0
