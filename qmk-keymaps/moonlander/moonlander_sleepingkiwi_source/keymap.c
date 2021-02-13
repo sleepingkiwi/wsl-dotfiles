e7923829c1607762fe5cd6147334929f794847c7
sleepingkiwi <joe@sleeping.kiwi>
2021-02-13 13:27:44 +0000

    Make bottom brackets make more sense, add lower ctrl

--- qmk-keymaps/moonlander/moonlander_sleepingkiwi_source/keymap.c
+++ qmk-keymaps/moonlander/moonlander_sleepingkiwi_source/keymap.c
@@ -55,8 +55,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LT(2,KC_TAB),   KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           WEBUSB_PAIR,                                    RGB_TOG,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_NONUS_BSLASH,
     KC_LCTRL,       KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_HYPR,                                                                        KC_MEH,         KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,       
     KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                           KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       RSFT_T(KC_ENTER),
-    KC_GRAVE,       UK_BSLS,        KC_LGUI,        KC_LALT,        LT(1,KC_SPACE), LT(1,KC_SPACE),                                                                                                 LT(1,KC_SPACE), LT(1,KC_SPACE), KC_LBRACKET,    KC_RBRACKET,    KC_MINUS,       KC_EQUAL,       
-    KC_LPRN,        KC_LBRACKET,    KC_LCBR,                        KC_RPRN,        KC_RBRACKET,    KC_RCBR
+    LCTL_T(KC_GRAVE),UK_BSLS,        KC_LGUI,        KC_LALT,        LT(1,KC_SPACE), LT(1,KC_SPACE),                                                                                                 LT(1,KC_SPACE), LT(1,KC_SPACE), KC_LBRACKET,    KC_RBRACKET,    KC_MINUS,       KC_EQUAL,       
+    KC_LPRN,        KC_LBRACKET,    KC_LCBR,                        KC_RCBR,        KC_RBRACKET,    KC_RPRN
   ),
   [1] = LAYOUT_moonlander(
     KC_TRANSPARENT, KC_F1,          KC_F2,          KC_F3,          KC_F4,          KC_F5,          KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         KC_DELETE,      
